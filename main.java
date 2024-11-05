b03ed0878a4b0944106c5e23b5fba5d1cc460221
golemiam <52841560+golemiam@users.noreply.github.com>
2024-11-04 20:36:55 -0700

    Update main.java
    Got started,

--- main.java
+++ main.java
@@ -1,1 +1,28 @@
-System....
+package project_3;
+
+public class project_3 {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        String[] valuesArray = {
+            Number: 1,2,3,4,5,6,7,8,9,10,11,12,
+            Experiment: "Cloud Patterns", "Solar Flares", "Solar Power", ""
+            Weight (in kilograms):
+            Rating""
+            1  36 5
+            2  264 9
+            3  188 6
+            4 Binary Stars 203 8
+            5 Relativity 104 8
+            6 Seed Viability 7 4
+            7 Sun Spots 90 2
+            8 Mice Tumors 65 8
+            9 Microgravity Plant
+            Growth
+            75 5
+            10 Micrometeorites 170 9
+            11 Cosmic Rays 80 7
+            12 Yeast Fermentation 27 4
+        }
+    };
+};
